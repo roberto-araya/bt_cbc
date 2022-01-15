405ffb541a5bf43f948b54f64f74f3ab2c3bed2e
Roberto Araya <roberto.araya@btester.cl>
2022-01-15 14:33:15 -0300

    Add block custom style form in layout builder.

---
+++ src/EventSubscriber/BlockComponentRenderArray.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Drupal\bt_cbc\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Drupal\layout_builder\LayoutBuilderEvents;
+use Drupal\layout_builder\Event\SectionComponentBuildRenderArrayEvent;
+
+/**
+ * Class SectionComponentRender.
+ */
+class BlockComponentRenderArray implements EventSubscriberInterface {
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    $events[LayoutBuilderEvents::SECTION_COMPONENT_BUILD_RENDER_ARRAY] = ['onBuildRender'];
+    return $events;
+  }
+
+  /**
+   * Adds block classes to section component.
+   *
+   * @param \Drupal\layout_builder\Event\SectionComponentBuildRenderArrayEvent $event
+   *   The section component render event.
+   */
+  public function onBuildRender(SectionComponentBuildRenderArrayEvent $event) {
+    $build = $event->getBuild();
+    if (!empty($build)) {
+      $build['#block_style'] = $event->getComponent()->getThirdPartySettings('block_style');
+      $event->setBuild($build);
+    }
+  }
+
+}
